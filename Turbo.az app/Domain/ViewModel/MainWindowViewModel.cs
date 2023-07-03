51d7b41eaf3baa4e62a8d76b83f4b21f27373807
ZenBook <ZenBook@DESKTOP-N379QS2>
2023-07-03 08:44:15 +0400

    Add project files.

---
+++ Turbo.az app/Domain/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turbo.az_app.Commands;
+using Turbo.az_app.Domain.ViewModel.UserControlViewModels;
+using Turbo.az_app.Domain.Views.UserControls;
+
+
+namespace Turbo.az_app.Domain.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public RelayCommand ShowCommand { get; set; }
+        public RelayCommand SelectionBrandCommand { get; set; }
+        public RelayCommand SelectionModelCommand { get; set; }
+        public RelayCommand AllCommand { get; set; }
+        public RelayCommand NewCommand { get; set; }
+        public RelayCommand OldCommand { get; set; }
+
+
+       
+        
+
+    }
+}
---
+++ Turbo.az app/Domain/Views/UserControls/CarInfoUC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Turbo.az_app.Domain.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CarInfoUC.xaml
+    /// </summary>
+    public partial class CarInfoUC : UserControl
+    {
+        public CarInfoUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
